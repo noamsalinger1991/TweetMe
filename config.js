974344591c7e9dda8729c9c0837e3069cd14f155
Your Namenoam <noamsalinger1991@gmail.com>
2017-07-05 13:38:24 +0300

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+exports.access_token  = '882171251615293440-U6HF36iH3xCaSsifUdz3uHaz351oHkj';
+exports.access_token_secret = 'VpxlqHWWEmP4cWEpoJ8ipHXbUR12rEnamEUEyQetdWrlq';
+exports.consumer_key = 'p5pS1ytxilcNYpSOPAPJM7V4j';
+exports.consumer_secret = 'QMpzvFxGITQmBCRGs5BdhXk5MwNJYIXdjMaC6lFgdnM7bhFWxQ';
