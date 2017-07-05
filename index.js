bae633727b26fcf7cd12715e04bab8cd3256907a
Your Namenoam <noamsalinger1991@gmail.com>
2017-07-05 16:52:58 +0300

    added tamplate start page

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+There's
