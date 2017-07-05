2aca9228711858243cbef14f1e067d126a2c3218
Your Namenoam <noamsalinger1991@gmail.com>
2017-07-05 21:29:35 +0300

    added functionality

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-var send = require('sendMessages.js');
-var get = require('getTweets.js');
 
 
 app.set('views', __dirname + '/views');
