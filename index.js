347917e08973b1a261ee63353b6cb1b52db9c513
Your Namenoam <noamsalinger1991@gmail.com>
2017-07-05 21:28:37 +0300

    added functionality

--- index.js
+++ index.js
@@ -9,11 +9,71 @@ app.set('views', __dirname + '/views');
 
 app.set('view engine', 'ejs');
 
-// set the home page route
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
+
+//Callback functions 
+var error = function (err, response, body) {
+    console.log('ERROR [%s]', err);
+};
+var success = function (data) {
+    console.log('Data [%s]', data);
+};
+
+var Twitter = require('twit');
+
+
+
+
+var config = require('./config');
+var twitter = new Twitter({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token: config.access_token,
+    access_token_secret: config.access_token_secret
+});
+
+function post(message) {
+    twitter.post('statuses/update', { status: message }, function (error, tweet, response) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(tweet);  // Tweet body.
+        console.log(response);  // Raw response object.
+    });
+}
+
+
+//Callback functions 
+var error = function (err, response, body) {
+    console.log('ERROR [%s]', err);
+};
+var success = function (data) {
+    console.log('Data [%s]', data);
+};
+
+var Twitter = require('twit');
+
+
+
+
+var config = require('./config');
+var twitter = new Twitter({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token: config.access_token,
+    access_token_secret: config.access_token_secret
+});
+
+
+function get(filter) {
+    twitter.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function (error, data, response) {
+        console.log(data)
+        if (error) {
+            console.log(error);
+        }
+        console.log(tweet);  // Tweet body.
+        console.log(response);  // Raw response object.
+    });
+} 
